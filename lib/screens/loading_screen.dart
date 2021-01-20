a4537cb7fe07ff01115ec4c8853319d539744aec
Hari Govind V <65269987+mercury-ai@users.noreply.github.com>
2021-01-20 22:31:58 +0530

    openweathermap api

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart' as http;
+import 'package:clima/services/networking.dart';
+
+const apiKey = '3540724434b2f81f3f79da0abdaa0491';
+
 
 
 class LoadingScreen extends StatefulWidget {
@@ -10,34 +13,28 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
+  double latitude;
+  double longitude;
+
   @override
   void initState(){
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getData() async{
-    String data;
-    http.Response  response = await http.get('http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=3540724434b2f81f3f79da0abdaa0491');
-    if(response.statusCode==200){
-      data = response.body;
-      print(response.body);
-    }else{
-      print(response.statusCode);
-    }
-  }
-
-  void getLocation() async{
+  void getLocationData() async{
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
-  }
+    latitude = location.latitude;
+    longitude = location.longitude;
 
+    NetworkHelper networkHelper = NetworkHelper('http://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    var weatherData = await networkHelper.getData();
+
+  }
 
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold(
       body: Center(
         child: Text(
